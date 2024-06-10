d376847b10532e7d835c95c6e0a866cb01ebd382
justinn1104 <just.andres1104@gmail.com>
2024-06-10 02:09:55 -0500

    Update PrincipalGrupoF.java

--- P1Proyecto0.2GF/src/SistemaParquin/PrincipalGrupoF.java
+++ P1Proyecto0.2GF/src/SistemaParquin/PrincipalGrupoF.java
@@ -1,10 +1,10 @@
 package SistemaParquin;
 
 public class PrincipalGrupoF {
-	static MotoGrupoF moto = new MotoGrupoF("","","","","","",""); 
-	public static void main(String[] args) {
-		moto.GenerarTiket();
-		
-	}
+    static MotoGrupoF moto = new MotoGrupoF("","","","","","","","");
+    static AutoGrupoF auto = new AutoGrupoF("","","","","","","","");
 
+    public static void main(String[] args) {
+        moto.MenuSystemParquin();
+    }
 }
