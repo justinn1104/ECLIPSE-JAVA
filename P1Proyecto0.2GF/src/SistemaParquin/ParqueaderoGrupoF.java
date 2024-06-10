a6b2120b35fcc59fecc63cea85c6d3f0dcb1518e
justinn1104 <just.andres1104@gmail.com>
2024-06-10 01:12:49 -0500

    Update ParqueaderoGrupoF.java

--- P1Proyecto0.2GF/src/SistemaParquin/ParqueaderoGrupoF.java
+++ P1Proyecto0.2GF/src/SistemaParquin/ParqueaderoGrupoF.java
@@ -1,15 +1,23 @@
 package SistemaParquin;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
 public abstract class ParqueaderoGrupoF {
 	static Scanner cin = new Scanner(System.in); 
-	static ParqueaderoGrupoF moto = new MotoGrupoF("","","","","","","");
-	static ParqueaderoGrupoF auto = new AutoGrupoF("","","","","","","");
+	static ParqueaderoGrupoF moto = new MotoGrupoF("","","","","","","","");
+	static ParqueaderoGrupoF auto = new AutoGrupoF("","","","","","","","");
 	private int cantidadPisos=2, cantidadBloques=15;
 	protected int dd, mm, yy, horas, minutos;
+	
 	protected String tipoVeiculo, fechaHora, espacioSelecc, numPlaca, nombreUser,telefono, direccion;//atributos comunes
-	protected String modelo, line,piso, bloque;
+	protected String modelo, line,piso, bloque, tipo;
 	protected int pisoSeleccion, bloqueSeleccion;
-	public ParqueaderoGrupoF(String tipoVeiculo, String fechaHora, String espacioSelecc, String numPlaca, String nombreUser, String telefono, String direccion) {
+	private String[][] espacios = new String[2][15];
+	public ParqueaderoGrupoF(String tipoVeiculo, String fechaHora, String espacioSelecc, String numPlaca, String nombreUser, String telefono, String direccion, String tipo) {
 		this.tipoVeiculo = tipoVeiculo;
 		this.fechaHora = fechaHora;
 		this.espacioSelecc =  espacioSelecc;
@@ -17,6 +25,7 @@ public abstract class ParqueaderoGrupoF {
 		this.nombreUser = nombreUser;
 		this.telefono = telefono;
 		this.direccion = direccion;
+		this.tipo = tipo;
 	}
 	public int getCantidadPisos() {
 		return cantidadPisos;
@@ -49,6 +58,7 @@ public abstract class ParqueaderoGrupoF {
 		}else {
 			piso = "B";
 		}
+		espacios[pisoSeleccion-1][bloqueSeleccion-1]="ocupado";
 		bloque=Integer.toString(bloqueSeleccion);
 		return piso+bloque;
 	}
@@ -130,29 +140,96 @@ public abstract class ParqueaderoGrupoF {
 	public String getBloque() {
 		return  bloque;
 	}
+	public void setEspacio(String espacio, int a, int b) {
+		this.espacios[a][b] = espacio;
+	}
 	public void MenuSystemParquin() {
 		int opMenuGf;
-		System.out.println("**SISTEMA DE PARQUEDERO M&M 24H**");
-		System.out.println("\n\t[1] REGISTRAR INGRESO - CREA&LEE -  CSVV&JSON");
-		System.out.println("\t[2] REGISTRAR SALIDA - CREA&LEE -  CSVV&JSON");
-		System.out.println("\t[3] SALIR");
-		System.out.print("\nSELECCIONA UNA OPCION [1 AL 3] :");
+		System.out.println("\t**SISTEMA DE PARQUEDERO M&M 24H**");
+		System.out.println("\n\t[1] INGRESO VEHICULO - CREA&LEE -  CSVV&JSON");
+		System.out.println("\t[2] SALIDA VEHICULO - CREA&LEE -  CSVV&JSON");
+		System.out.println("\t[3] CONSULTAR VEHICULO - LEE - CSV&JSON ");
+		System.out.println("\t[4] SALIR");
+		System.out.print("\nSELECCIONA UNA OPCION [1 AL 4] :");
 		opMenuGf = cin.nextInt();
 		switch(opMenuGf) {
 		case 1:
-			moto.GenerarTiket();
-			auto.GenerarTiket();
+			IngresoVeiculos();
 			MenuSystemParquin();
 			break;
 		case 2:
 			MenuSystemParquin();
 			break;
 		case 3:
+			consultarVehiculo();
+			MenuSystemParquin();
+			break;
+		case 4:
 			System.out.println("**SISTEMA CERRADO**");
 			break;
 		default:
 			System.out.println("**VUELVA A SELECCIONAR UNA OPCION**");
 			MenuSystemParquin();
 		}
-	}	
+	}
+	public void consultarVehiculo() {
+		String placa;
+		System.out.print("INGRESA EL NUMERO DE LA PLACA VEICULAR [6 A 7]DIGITOS: ");
+		placa = cin.next();
+		while(placa.length()<6||placa.length()>7) {
+			System.out.print("**VUELVA A INGRESAR EL NUMERO DE LA PLACA VEICULAR [6 A 7]DIGITOS**:");
+			placa = cin.next();
+		}
+		placa.toLowerCase();
+		System.out.println("\nLECTURA DEL ARCHIVO .CSV\n");
+		try (BufferedReader leerCsv = new BufferedReader(new FileReader(placa+".csv"))){
+			while((line = leerCsv.readLine())!=null) {
+				System.out.println(line);
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\nLECTURA DEL ARCHIVO .JSON\n");	
+		try(FileReader fileReader = new FileReader (placa+".json")){
+			JSONObject jsonReader = new JSONObject (new JSONTokener(fileReader));
+			System.out.println("Vehiculo: " +jsonReader.getString("Vehiculo: "));
+			System.out.println("Nombre de usuario: " +jsonReader.getString("Nombre de usuario: "));
+			System.out.println("Modelo: " +jsonReader.getString("Modelo: "));
+			System.out.println("Numero de placa: " +jsonReader.getString("Numero de placa: "));
+			System.out.println("Telefono: " +jsonReader.getString("Telefono: "));
+			System.out.println("Direccion: " +jsonReader.getString("Direccion: "));
+			System.out.println("Fecha y hora : " +jsonReader.getString("Fecha y hora : "));
+			System.out.println("Espacion seleccionado: " +jsonReader.getString("Espacion seleccionado: "));
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void IngresoVeiculos() {
+		int opMenuGf;
+		do {
+			System.out.println("\n\t**INGRESO DE VEHIVULOS**\n"
+					+ "\n\t[1] MOTO"
+					+ "\n\t[2] AUTO"
+					+ "\n\t[3] REGRESAR"
+					+ "\n\nSELECCIONA EL TIPO DEL VEHICULO:");
+			opMenuGf = cin.nextInt();
+			switch(opMenuGf) {
+			case 1:
+				moto.GenerarTiket();
+				break;
+			case 2:
+				auto.GenerarTiket();
+				break;
+			case 3:
+				System.out.println("**REGRASTE AL MENU PRINCIPAL**");
+				break;
+			default:
+				System.out.println("**OPCION INCORRECTA**");
+			}
+		}while(opMenuGf !=3);
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 }
